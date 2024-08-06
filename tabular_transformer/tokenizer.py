fbdad11f4f1536b4cb303669638dca66a089bb26
QIAO QIAN <qiaoqianda@gmail.com>
2024-08-06 10:07:29 +0800

    remove long comments

--- tabular_transformer/tokenizer.py
+++ tabular_transformer/tokenizer.py
@@ -8,74 +8,6 @@ from .util import FeatureType, SCALAR_NUMERIC, SCALAR_UNK, CATEGORICAL_UNK
 
 class Tokenizer():
 
-    """
-    sample tabular data, [UNK] means the value is missing or anomaly, we treated as UNKNOWN
-    +---------+-------+---------------+---------------+
-    | Name    | Age   | City          | Income        |
-    +---------+-------+---------------+---------------+
-    | Alice   | 30    | [UNK]         | 500,000       |
-    | Bob     |[UNK]  | San Francisco | 1,000,000     |
-    | [UNK]   | 35    | Los Angeles   | 200,000       |
-    | Charlie | 35    | Los Angeles   | [UNK]         |
-    +---------+-------+---------------+---------------+
-
-    1. Every feature(column) must be one of the two types:
-        a) Scalar variables  
-            such as height, weight, the number of floors in a building
-        b) Categorical variables
-            such as race, gender, place of birth, color, type of medication
-
-    2. do some statics to calcuate the feature size, for less frequent categorical value, replace it with [UNK]
-
-        the feature size for the tabular is 11
-
-        FEATURE VOCAB DICTIONARY
-        +--------------+-------+
-        | feature      | index |
-        +--------------+-------+
-        | Name_[UNK]   | 0     |
-        | Name_Alice   | 1     |
-        | Name_Bob     | 2     |
-        | Name_Charlie | 3     |
-        | Age_[UNK]    | 4     |
-        | Age_num      | 5     |
-        | City_[UNK]   | 6     |
-        | City_SF      | 7     |
-        | City_LA      | 8     |
-        | Income_[UNK] | 9     |
-        | Income_num   | 10    |
-        +--------------+-------+
-
-    3. look up the feature dictionary for feature tokens, datatype is integer
-        feature_tokens for next step EMBEDDING
-
-                    FEATURE TOKENS
-    +---------+-------+---------------+---------------+
-    | Name    | Age   | City          | Income        |
-    +---------+-------+---------------+---------------+
-    | 1       | 5     | 6             | 10            |
-    | 2       | 4     | 7             | 10            |
-    | 0       | 5     | 8             | 10            |
-    | 3       | 5     | 8             | 9             |
-    +---------+-------+---------------+---------------+
-
-    4. fill in the feature weights, datatype is float
-       the Categorical variable fill 1.0
-       the Scalar variable(except [UNK]) fill normalized value
-
-                    FEATURE WEIGHTS
-    +---------+-------+---------------+---------------+
-    | Name    | Age   | City          | Income        |
-    +---------+-------+---------------+---------------+
-    | 1.0     |-1.1547| 1.0           | -0.1650       |
-    | 1.0     |1.0    | 1.0           | 1.0722        |
-    | 1.0     |0.5774 | 1.0           | -0.9073       |
-    | 1.0     |0.5774 | 1.0           | 1.0           |
-    +---------+-------+---------------+---------------+
-
-
-    """
-
     feature_vocab: Dict[str, int]
     feature_type: Dict[str, FeatureType]
 
@@ -126,7 +58,7 @@ class Tokenizer():
                 feature_tokens_list.append(t1)
                 feature_weight_list.append(v1)
             else:
-                raise ValueError("column type can be CATEGORICAL or SCALAR")
+                raise ValueError("column type can be CATEGORICAL or NUMERICAL")
 
         return torch.stack(feature_tokens_list, dim=1), torch.stack(feature_weight_list, dim=1)
 
