eabbf0d2ef8c2efe278d596ede527c66abe71e1a
QIAO QIAN <qiaoqianda@gmail.com>
2024-08-16 16:21:20 +0800

    add accuracy

--- tabular_transformer/metrics.py
+++ tabular_transformer/metrics.py
@@ -3,37 +3,40 @@ from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, 
 
 
 def calAUC(y_true, y_pred):
-    return round(roc_auc_score(y_true, y_pred), 4)
+    return roc_auc_score(y_true, y_pred)
 
 
 def calF1Macro(y_true, y_pred):
-    return round(f1_score(y_true, y_pred, average='macro'), 4)
+    return f1_score(y_true, y_pred, average='macro')
 
 
 def calF1Micro(y_true, y_pred):
-    return round(f1_score(y_true, y_pred, average='micro'), 4)
+    return f1_score(y_true, y_pred, average='micro')
 
 
 def calMAPE(y_true, y_pred):
-    return round(mean_absolute_percentage_error(y_true, y_pred)*100, 2)
+    return mean_absolute_percentage_error(y_true, y_pred)
 
 
 def calMAE(y_true, y_pred):
-    return round(mean_absolute_error(y_true, y_pred)/10000, 2)
+    return mean_absolute_error(y_true, y_pred)
 
 
 def calMSE(y_true, y_pred):
-    return round(mean_squared_error(y_true, y_pred)/10000, 2)
+    return mean_squared_error(y_true, y_pred)
 
 
 def calRMSE(y_true, y_pred):
-    return round(np.sqrt(mean_squared_error(y_true, y_pred))/10000, 2)
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
+def calAccuracy(y_true, y_pred):
+    accuracy = np.mean(y_true == y_pred)
+    return accuracy
 
 
 def calHitRate(y_hat, y, rate):
-    hit, total = 0, 0
-    for each_y_hat, each_y in zip(y_hat, y):
-        if np.abs((each_y_hat-each_y)/each_y) < rate:
-            hit += 1
-        total += 1
-    return round((hit/total)*100, 2)
+    relative_error = np.abs((y_hat - y) / y)
+    hit = np.sum(relative_error < rate)
+    total = len(y)
+    return hit / (total + 1e-8)
