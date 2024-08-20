60766f157171a1982f03e938fb05e95ac4813496
QIAO QIAN <qiaoqianda@gmail.com>
2024-08-20 14:45:34 +0800

    add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='tabular-transformer',
     packages=find_packages(),
     version='0.1.0',
     license='MIT',
     description='Transformer adapted for tabular data domain',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Qiao Qian',
     author_email='qiaoqianda@gmail.com',
