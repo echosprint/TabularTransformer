dab70e585b047f78172e2511b029030e858bd2c9
QIAO QIAN <qiaoqianda@gmail.com>
2024-08-22 10:40:49 +0800

    higgs dataset

--- tabular_transformer/__init__.py
+++ tabular_transformer/__init__.py
@@ -3,5 +3,10 @@ from tabular_transformer.tabular_transformer import TabularTransformer
 from tabular_transformer.dataloader import Task
 from tabular_transformer.data_common import DataReader
 from tabular_transformer.trainer import Trainer
-from tabular_transformer.util import prepare_income_dataset, prepare_fish_dataset, prepare_iris_dataset, download_notebooks
+from tabular_transformer.util import (
+    prepare_income_dataset,
+    prepare_fish_dataset,
+    prepare_iris_dataset,
+    download_notebooks,
+    prepare_higgs_dataset)
 from tabular_transformer.predictor import Predictor
