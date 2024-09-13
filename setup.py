d784900196271a879756e1a60a9e80e2b30c9bd1
QIAO QIAN <qiaoqianda@gmail.com>
2024-09-13 15:42:07 +0800

    version 0.2.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='tabular-transformer',
     packages=find_packages(),
-    version='0.2.6',
+    version='0.2.7',
     license='MIT',
     description='Transformer adapted for tabular data domain',
     long_description=long_description,
